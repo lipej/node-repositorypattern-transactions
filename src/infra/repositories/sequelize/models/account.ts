0ba8ed3c25e907248785b4ac2ae750c69a4cf520
Felipe José <lipejose@icloud.com>
2022-07-09 17:37:23 -0300

    feat: sequelize model

---
+++ src/infra/repositories/sequelize/models/account.ts
@@ -0,0 +1,19 @@
+import { Sequelize, Model, DataTypes } from "sequelize";
+
+export const sequelize = new Sequelize("sqlite::memory:");
+
+export const AccountModel = sequelize.define('Account', {
+  name: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  email: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  balance: {
+    type: DataTypes.NUMBER,
+    defaultValue: 0
+  }
+});
+
